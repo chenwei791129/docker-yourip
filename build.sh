155e5a7e80ea764e2a6bb5dfa72b859b8643fa53
AwEi <chenwei791129@livemail.tw>
2023-06-03 22:47:54 +0800

    change to use podman instead of docker-cli

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TAG=$1
-docker build --no-cache --rm -t awei/yourip .
-docker tag awei/yourip:latest awei/yourip:${TAG}
-docker push awei/yourip:latest
-docker push awei/yourip:${TAG}
+podman build --no-cache --rm -t awei/yourip .
+podman tag awei/yourip:latest awei/yourip:${TAG}
+podman push awei/yourip:latest
+podman push awei/yourip:${TAG}
